a85d72ae2752f6efe8f141cbfc28a860b1bc7f37
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-04-22 11:14:29 +0100

    changed dependencies

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,6 +1,7 @@
 const path = require("path");
-// const express = require("express");
-const router = require.Router();
+// const db = require("../models");
+const express = require("express");
+const router = express.Router();
 
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
