6df7c8d59d27b9ff9c370f5cf791265548635505
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-04-22 11:14:05 +0100

    added routes to get, post and put

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,16 +1,62 @@
 const express = require("express");
+const router = express.Router();
 const mongoose = require("mongoose");
-const router = require.Router();
 const Workout = require("../models/workout.js");
 
+// Find all exercises
 router.get('/api/workouts', (req, res) => {
     Workout.find({})
-    .then((dbWorkout) => {
-        res.json(dbWorkout);
-    })
-    .catch((err) => {
-        res.json(err);
-    });
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+// Find a range/limit of exercises
+router.get('/api/workouts/range', (req, res) => {
+    Workout.find({})
+        .limit(7)
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+// Add a new exercise
+router.post('/api/workouts', ({ body }, res) => {
+    Workout.create({})
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+// Update an existing exercise
+router.put('/api/workouts/:id', ({ params, body }, res) => {
+    Workout.findByIdAndUpdate(
+        {
+            _id: params.id
+        },
+        {
+            $push: { exercises: body }
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+        .then((dbWorkout) => {
+            res.json(dbWorkout)
+        })
+        .catch((err) => {
+            res.json(err);
+        });
 });
 
 module.exports = router;
