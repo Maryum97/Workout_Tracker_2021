a8bd8e30d0ada93acf8a8ef069240d191bb6c729
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-04-23 18:35:55 +0100

    added code for mongoose connection

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ app.use(apiRoutes);
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 mongoose.connect(MONGODB_URI,{  
-    useNewUrlParser:true,
-    useFindAndModify:false
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 })
 
 app.listen(PORT, () => {
