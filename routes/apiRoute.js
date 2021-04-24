9810d91a1efab526ac57685b7d93daf24f754227
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-04-24 21:10:32 +0100

    random edits

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -22,6 +22,7 @@ router.get('/api/workouts/range', (req, res) => {
             res.json(dbWorkout);
         })
         .catch((err) => {
+            console.log(err);
             res.json(err);
         });
 });
